70ecf1018aa6baf24c05834092a9a79c320cc293
Francisco Ortega <mandor_ort@hotmail.com>
2026-05-15 17:22:42 +0000

    feat(dev): Primeros pasos de la capa silver, donde normalizamos y creamos tablas estaticas de referencia

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -6,7 +6,7 @@
         {{ default_schema }}
 
     {%- elif target.name == 'dev' -%}
-        {{ default_schema }}_{{ custom_schema_name | trim }}
+        {{ custom_schema_name | trim }}
 
     {%- else -%}
         {{ custom_schema_name | trim }}
